ea48112549163405bfec5ac94d5af776ccec029b
G1edriusP <giedrius.pakalniskis@gmail.com>
2022-08-12 16:02:21 +0300

    Sentry setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 
+// Components
+import * as Sentry from "@sentry/react-native";
+
 // Screens
 import Home from "./src/screens/Home";
+
+// Initialize error monitoring
+Sentry.init({
+  dsn: "https://2bb78eab9a0b4af2bbf0cf2571e225c9@o1355877.ingest.sentry.io/6640749",
+});
 
 const App = () => {
   return <Home />;
 };
 
-export default App;
+export default Sentry.wrap(App);
